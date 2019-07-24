0ee26f646b8c61bf56d5c9af3bbe4c4057a8239c
Tom Brougham <tbrougham@paravian.net>
2019-07-24 18:23:31 +1000

    Fixes for cross platform usage

--- R/runTnt.R
+++ R/runTnt.R
@@ -9,23 +9,16 @@
 runTnt <- function (tnt.path, matrix, cmds) {
   tnt.tempfile <- tempfile("nitro", fileext = ".tnt")
   tnt.matrix <- as.character(matrix)
-  write.nexus.data(tnt.matrix, file=tnt.tempfile, interleaved = FALSE, format = "standard")
 
-  tnt.block <- paste(cmds, collapse="\n", sep="\n") %>%
-    paste(., "tplot *;", "length;", "minmax;", sep="\n") %>%
-    paste("BEGIN TNT;", ., "END;", sep="\n")
+  write.nexus.data(tnt.matrix, file=tnt.tempfile, interleaved = FALSE,
+                   format = "standard")
+
+  tnt.block <- paste(c("BEGIN TNT;", "log stdout;", cmds, "tplot *;",
+                       "length;", "minmax;", "END;"), collapse = "\n")
 
   write(tnt.block, file=tnt.tempfile, append=TRUE)
 
-  platform <- tolower(.Platform$OS.type)
-
-  if (platform == "unix") {
-    output <- system2(normalizePath(tnt.path),
-                      stdout = TRUE,
-                      stderr = TRUE,
-                      input=paste0("proc ", tnt.tempfile, ";"), env=c("COLUMNS=10000"))
-    return(output)
-  } else if (platform == "windows") {
-    stop("Sorry, not available for your platform!")
-  }
+  output <- system2(normalizePath(tnt.path), stdout = TRUE, stderr = FALSE,
+                    input=paste0("proc ", tnt.tempfile, ";"))
+  return(output)
 }
