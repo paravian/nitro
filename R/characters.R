b0343cb78825ce3eaf5d1860d4b80a0804a10946
Tom Brougham <tbrougham@paravian.net>
2019-09-03 09:28:26 +1000

    Add ordered character function

---
+++ R/characters.R
@@ -0,0 +1,24 @@
+#' Define ordered characters
+#'
+#' @param matrix A \code{phyDat} object of USER type.
+#' @param charactes A vector of characters to be set as ordered.
+#' @return A \code{phyDat} object.
+orderedCharacters <- function (matrix, characters) {
+  if (class(matrix) != "phyDat") {
+    stop("'matrix' must be a phyDat object")
+  }
+  if (attr(matrix, "type") != "USER") {
+    stop("Cannot apply ordering to sequences of type DNA, AA or CODON")
+  }
+  if (is.numeric(characters) == FALSE) {
+    stop("'characters' must be a numeric")
+  } else if (any(characters %% 1 != 0) | any(characters < 1)) {
+    stop("'characters' can only contain positive integers > 0")
+  }
+  if (any(characters > attr(matrix, "nr"))) {
+    stop(paste("Values of 'characters' exceed number of characters in the matrix (", attr(matrix, "nr"), ")"))
+  }
+
+  attr(matrix, "ordered") <- 1:attr(matrix, "nr") %in% characters
+  return(matrix)
+}
