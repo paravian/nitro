606f9319e3080cde06c1a2e0c9bc7ef2eda2ed35
paravian <contact@paravian.net>
2019-09-03 09:25:00 +1000

    New output format

--- R/driven.R
+++ R/driven.R
@@ -2,40 +2,55 @@
 #'
 #' @param tnt.path The location of the TNT command-line binary.
 #' @param matrix A \code{phyDat} object of the matrix.
+#' @param run.now Logical; perform a phylogenetic analysis straight away or save
+#'   commands for use in other methods
 #' @param hold The maximum number of trees to allow TNT to hold in memory.
-#' @param outgroup The outgroup taxon for the phylogenetic analysis. By default, the first taxon in the matrix is considered the outgroup.
-#' @param hits The number of times to reach the shortest tree for each replication.
+#' @param outgroup The outgroup taxon for the phylogenetic analysis. By default,
+#'   the first taxon in the matrix is considered the outgroup.
+#' @param hits The number of times to reach the shortest tree for each
+#'   replication.
 #' @param replications The number of replications.
 #' @param rss,css,xss Use random, constraint or exclusive sectorial searches.
 #' @param ratchet.cycles The number of cycles of the parsimony ratchet.
 #' @param drifting.cycles The number of cycles of tree drifting.
 #' @param fusing.rounds The number of rounds of fusing.
-#' @param consense.times Number of times to consense until the consensus is stablilised.
-#' @param multiply After hitting target score, find additional trees by fusing suboptimal with optimal trees
-#' @param hold.rep The maximum number of trees to retain during each replication.
-#' @param keepall Retain all generated trees from each replication regardless of length. This has a different
-#' meaning when \code{hits} = 1 and when \code{hits} > 1. When \code{hits} = 1, it is trees from each of the
-#' RAS + TBR +  SS or DFT or RAT, in addition to the trees resulting from fusing those.  When \code{hits} > 1,
-#' then it means the trees resulting from fusing the initial starting trees for each of starting points.
-#' @return A \code{multiPhylo} object.
+#' @param consense.times Number of times to consense until the consensus is
+#'   stablilised.
+#' @param multiply After hitting target score, find additional trees by fusing
+#'   suboptimal with optimal trees
+#' @param hold.rep The maximum number of trees to retain during each
+#'   replication.
+#' @param keepall Retain all generated trees from each replication regardless of
+#'   length. This has a different meaning when \code{hits} = 1 and when
+#'   \code{hits} > 1. When \code{hits} = 1, it is trees from each of the RAS +
+#'   TBR +  SS or DFT or RAT, in addition to the trees resulting from fusing
+#'   those.  When \code{hits} > 1, then it means the trees resulting from fusing
+#'   the initial starting trees for each of starting points.
+#' @return A list containing the search parameters and TNT command string and,
+#'   if \code{run.now} is \code{TRUE}, a \code{multiPhylo} object of trees
+#'   found from the search commands.
 #' @export
-driven <- function(tnt.path, matrix, hold=100, outgroup=NULL, hits=1, replications=4, hold.rep=1,
-                   autoconstrain="wagner", rss=TRUE, css=FALSE, xss=FALSE, ratchet.cycles=0,
+driven <- function(tnt.path, matrix, run.now=TRUE, hold=100, outgroup=NULL,
+                   hits=1, replications=4, hold.rep=1, autoconstrain="wagner",
+                   rss=TRUE, css=FALSE, xss=FALSE, ratchet.cycles=0,
                    drifting.cycles=30, hybridization=FALSE, fusing.rounds=0,
                    consense.times=0, multiply=FALSE, keepall=FALSE) {
-
   if (file_test("-f", tnt.path) == FALSE) {
     stop("'tnt.path' does not exist")
   }
   if (class(matrix) != "phyDat") {
     stop("'matrix' must be a phyDat object")
   }
+  if (is.logical(run.now) == FALSE) {
+    stop("'run.now' must be a logical")
+  }
   if (is.numeric(hold) == FALSE | length(hold) != 1) {
     stop("'hold' must be an integer")
   } else if (hold %% 1 != 0 | hold <= 0) {
     stop("'hold' must be an integer > 0")
   }
-  if ((is.null(outgroup) == TRUE | is.integer(outgroup) == TRUE) == FALSE & length(outgroup) != 1) {
+  if ((is.null(outgroup) == TRUE | is.integer(outgroup) == TRUE) == FALSE &
+      length(outgroup) != 1) {
     stop("'outgroup' must be a character")
   }
   if(is.numeric(hits) == FALSE | length(hits) != 1) {
@@ -88,27 +103,32 @@ driven <- function(tnt.path, matrix, hold=100, outgroup=NULL, hits=1, replicatio
     stop("'keepall' must be a logical")
   }
 
-  tnt.cmds <- list(paste("hold", hold, ";"))
+  tnt.params <- list(hold = hold, outgroup = outgroup, hits = hits,
+                     replications = replications)
 
-  if (!is.null(outgroup)) {
-    tnt.cmds <- append(tnt.cmds, paste("outgroup", outgroup, ";"))
+  tnt.params$cmd <- c(paste("xmult=", "hits", hits, "replications",
+                            replications, "hold", hold.rep,
+                            ifelse(rss, "rss", "norss"),
+                            ifelse(css, "css", "nocss"),
+                            ifelse(xss, "xss", "noxss"),
+                            ifelse(fusing.rounds == 0, "nofuse",
+                                   paste("fuse", fusing.rounds)),
+                            ifelse(ratchet.cycles == 0, "noratchet",
+                                   paste("ratchet", ratchet.cycles)),
+                            ifelse(drifting.cycles == 0, "nodrift",
+                                   paste("drift", drifting.cycles)),
+                            ifelse(consense.times == 0, "noconsense",
+                                   paste("consense", consense.times)),
+                            ifelse(multiply, "multiply", "nomultiply"),
+                            ifelse(hybridization, "hybrid", "nohybrid"),
+                            ifelse(keepall, "keepall", "nokeepall"),
+                            ";"))
+
+  if (run.now) {
+    output <- runTnt(tnt.path, matrix, tnt.params)
+    trees <- tntTreeParse(output, names(matrix))
+    return(list(tnt.params = tnt.params, trees = trees))
+  } else {
+    return(list(tnt.params = tnt.params))
   }
-
-  tnt.cmds <- c(tnt.cmds,
-                c(paste("hold", hold, ";"),
-                  paste("xmult=", "hits", hits, "replications", replications, "hold",
-                        hold.rep, paste0(rep("no", !rss), "rss"),
-                        paste0(rep("no", !css), "css"),
-                        paste0(rep("no", !xss), "xss"),
-                        paste0(ifelse(fusing.rounds == 0, "no", ""), "fuse ", ifelse(fusing.rounds == 0, "", fusing.rounds)),
-                        paste0(ifelse(ratchet.cycles == 0, "no", ""), "ratchet ", ifelse(ratchet.cycles == 0, "", ratchet.cycles)),
-                        paste0(ifelse(drifting.cycles == 0, "no", ""), "drift ", ifelse(drifting.cycles == 0, "", drifting.cycles)),
-                        paste0(ifelse(consense.times == 0, "no", ""), "consense ", ifelse(consense.times == 0, "", consense.times)),
-                        paste0(ifelse(!multiply, "no", ""), "multiply"),
-                        paste0(ifelse(!hybridization, "no", ""), "hybrid"),
-                        paste0(ifelse(!keepall, "no", ""), "keepall", ";"))))
-
-  output <- runTnt(tnt.path, matrix, tnt.cmds)
-  trees <- tntTreeParse(output, names(matrix))
-  return(trees)
 }
