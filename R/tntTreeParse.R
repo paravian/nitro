63144abb110ac6d36ec2d6cc366ce4b93dc39908
Tom Brougham <tbrougham@paravian.net>
2019-09-08 19:34:35 +1000

    Save weighted scores

--- R/tntTreeParse.R
+++ R/tntTreeParse.R
@@ -9,6 +9,7 @@ tntTreeParse <- function (tnt.output, tip.labels) {
   # Get start lines for tree strings and lengths output
   first.trees <- grep("Tread 'set of [0-9]+ trees' ", tnt.output) + 1
   tree.lengths <- grep("Tree lengths ", tnt.output)
+  weighted.scores <- grep("Adjusted homoplasy ", tnt.output)
 
   # Convert TNT's tree strings into newick format
   newick.trees <- paste(tnt.output[first.trees:(tree.lengths - 2)],
@@ -30,20 +31,35 @@ tntTreeParse <- function (tnt.output, tip.labels) {
   maxsteps <- as.numeric(gsub(".* ([0-9]+)\\) ", "\\1",
                               tnt.output[maxsteps.match]))
 
-  # Get all tree lengths
-  lengths <- tnt.output[(tree.lengths+3):(minsteps.match-3)] %>%
-    .[c(TRUE, rep(FALSE, 2))] %>%
-    gsub("^ +", "", .) %>%
-    strsplit(" +") %>%
-    unlist
+  tableParse <- function (table) {
+    return(table[c(TRUE, rep(FALSE, 2))] %>%
+      gsub("^ +", "", .) %>%
+      strsplit(" +") %>%
+      unlist
+    )
+  }
 
-  # Create phylo objects from newick trees; calculate statistics
-  trees <- apply(cbind(newick.trees, lengths), 1, function (tree.data) {
+  # Get all tree lengths
+  lengths <- tableParse(tnt.output[(tree.lengths+3):(minsteps.match-3)])
+
+  tree.attrs <- cbind(newick.trees, lengths)
+
+  scores <- NULL
+  if (length(weighted.scores)) {
+    scores <- tableParse(tnt.output[(weighted.scores+3):length(tnt.output)])
+    tree.attrs <- cbind(tree.attrs, scores)
+  }
+
+    # Create phylo objects from newick trees; calculate statistics
+  trees <- apply(tree.attrs, 1, function (tree.data) {
     tree <- read.tree(text=tree.data[1])
     tree$tip.label <- tip.labels[as.numeric(tree$tip.label) + 1]
-    tree$score <- as.numeric(tree.data[2])
-    tree$CI <- minsteps / tree$score
-    tree$RI <- (maxsteps - tree$score) / (maxsteps - minsteps)
+    tree$length <- as.numeric(tree.data[2])
+    if (length(tree.data) > 2) {
+      tree$weighted.score <- as.numeric(tree.data[3])
+    }
+    tree$CI <- minsteps / tree$length
+    tree$RI <- (maxsteps - tree$length) / (maxsteps - minsteps)
     tree$RC <- tree$CI * tree$RI
     return(tree)
   })
