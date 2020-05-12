b6e13a276add5131cff39d13301546fbf2d068d6
Tom Brougham <tbrougham@paravian.net>
2020-05-12 14:03:48 +1000

    Update tnt and parsing functions

--- R/runTnt.R
+++ R/runTnt.R
@@ -1,106 +1,186 @@
 #' Executes TNT commands
 #'
-#' @importFrom processx run
 #' @importFrom ape write.nexus.data
+#' @importFrom processx run
+#' @importFrom progress progress_bar
+#' @importFrom utils tail
 #' @description Executes commands on a supplied phylogenetic matrix in the TNT
 #'   command line binary.
-#' @param params the parameters for an analysis as specified from the output
-#'   of the \code{implicit.enum}, \code{branchswap}, \code{ratchet}, or
-#'   \code{driven} commands.
-#' @param tnt.path the location of the TNT command line binary.
+#' @param obj an object that inherits from \code{NitroBase} (i.e.,
+#'   \code{IterativeEnum}, \code{BranchSwap}, \code{Ratchet} or \code{Driven}.
+#' @param tnt_path the location of the TNT command line binary.
+#' @param hold an integer indicating the number of trees to hold in TNTs tree
+#'   buffer.
+#' @param max_ram a numeric indicating the number of (binary) megabytes to
+#'   allocate for use by TNT.
 #' @return An object containing the TNT parameters and the \code{phyDat} matrix
-#'   used in the analysis, and a \code{multiPhylo} containing the trees found
+#'   used in the analysis, and a \code{multiPhylo} containing the trees foundas()
 #'   during the search.
 #' @export
-tnt <- function (params, tnt.path) {
+tnt <- function (obj, tnt_path, hold, max_ram = 16) {
+  if (!inherits(obj, "NitroBase")) {
+    stop("'obj' must inherit class NitroBase")
+  }
+  if (!is.numeric(hold)) {
+    stop("'hold' must be of class numeric")
+  } else if (hold < 0 | hold %% 1 != 0) {
+    stop("'hold must be an integer greater than zero")
+  }
   # Read matrix; convert NAs to "?" and write to temporary minimal nexus file
-  tnt.tempfile <- tempfile("nitro", fileext = ".tnt")
-  tnt.matrix <- as.character(params$matrix)
-  tnt.matrix[is.na(tnt.matrix)] <- "?"
-  write.nexus.data(tnt.matrix, file=tnt.tempfile, interleaved = FALSE,
+  tnt_tempfile <- tempfile("nitro", fileext = ".tnt")
+  tnt_matrix <- obj@matrix
+  tnt_matrix[is.na(tnt_matrix)] <- "?"
+  write.nexus.data(tnt_matrix, file=tnt_tempfile, interleaved = FALSE,
                    format = "standard")
-
-  tnt.cmds <- c(paste0("collapse ", params$tnt.params$collapse, ";"))
-  if (!is.null(params$tnt.params$hold)) {
-    tnt.cmds <- c(tnt.cmds, paste0("hold ", params$tnt.params$hold, ";"))
-  }
-  if (!is.null(params$tnt.params$outgroup)) {
-    tnt.cmds <-
-      c(tnt.cmds, paste0("outgroup ",
-                         which(names(params$matrix) == params$tnt.params$outgroup) - 1, ";"))
-  }
-
-  char.codes <- c()
-  if (!is.null(attr(params$matrix, "ordered"))) {
-    char.codes <- c(char.codes, "+",
-                    which(attr(params$matrix, "ordered")) - 1)
-  }
-  if (!is.null(attr(params$matrix, "inactive.characters"))) {
-    char.codes <- c(char.codes, "]",
-                    which(attr(params$matrix, "inactive.characters")) - 1)
-  }
-  if (length(char.codes)) {
-    tnt.cmds <- c(tnt.cmds, paste(c("ccode", char.codes, ";"), collapse=" "))
-  }
-
-  if (!is.null(attr(params$matrix, "inactive.taxa"))) {
-    tnt.cmds <- c(tnt.cmds, paste(c("taxcode -",
-                                    which(attr(params$matrix, "inactive.taxa")) - 1, ";"), collapse=" "))
-  }
-
-  tnt.cmds <- c(tnt.cmds, params$tnt.params$cmd)
 
   # Prepare command line arguments for the TNT binary
   platform <- .Platform$OS.type
-  tnt.arg <- paste0("proc ", tnt.tempfile,
-                    ifelse(platform == "windows", ":", ","))
+  tnt_arg <- c(paste0("mxram", max_ram / 1000 * 1024 ),
+               paste0("proc ", tnt_tempfile,
+                      ifelse(platform == "windows", ":", ",")))
 
-  tnt.block <- c("BEGIN TNT;", "log stdout;", "tables =;", tnt.cmds,
-                 "condense;", "tplot *;", "length;", "minmax;")
+  tnt_block <- c("BEGIN TNT;", "log stdout;", "tables =;")
 
-  if (!is.null(params$tnt.params$iw.cmd)) {
-    tnt.arg <- c(params$tnt.params$iw.cmd, tnt.arg)
-    tnt.block <- c(tnt.block, "score;")
-    if (!is.null(params$tnt.params$eiw.cmd)) {
-      tnt.cmds <- c(params$tnt.params$eiw.cmd, tnt.cmds)
+  if (inherits(obj, "NitroImpliedWeights")) {
+    tnt_arg <- c(paste0("piwe =", obj@k, ";"), tnt_arg)
+    if (obj@multi_k) {
+      tnt_block <- c(tnt_block, "xpiwe (*;")
     }
   }
 
-  tnt.block <- c(tnt.block, "END;")
+  tnt_block <- c(tnt_block,
+    paste0("collapse ", collapse(obj), ";"),
+    paste0("hold ", as.integer(hold), ";"),
+    paste0("outgroup ",
+           which(rownames(obj@matrix) == outgroup(obj)) - 1, ";")
+  )
 
-  write(tnt.block, file=tnt.tempfile, append=TRUE)
+  char_codes <- c()
+  if (any(ordered_characters(obj))) {
+    char_codes <- c(char_codes, "+",
+                    which(ordered_characters(obj)) - 1)
+  }
+  if (any(inactive_characters(obj))) {
+    char_codes <- c(char_codes, "]",
+                    which(inactive_characters(obj)) - 1)
+  }
+  if (length(char_codes)) {
+    tnt_block <- c(tnt_block, paste(c("ccode", char_codes, ";"), collapse=" "))
+  }
+
+  if (any(inactive_taxa(obj))) {
+    tnt_block <- c(tnt_block,
+                   paste(c("taxcode -",
+                           which(inactive_taxa(obj)) - 1, ";"), collapse=" "))
+  }
+
+  tnt_block <- c(tnt_block, tnt_cmd(obj), "condense;", "tplot *;", "length;",
+                 "minmax;")
+
+  if (inherits(obj, "NitroImpliedWeights")) {
+    tnt_block <- c(tnt_block, "score;")
+  }
+
+  tnt_block <- c(tnt_block, "END;")
+
+  write(tnt_block, file=tnt_tempfile, append=TRUE)
 
   # Initialise progress bar
-  params$progress$bar$tick(0)
+  pb_setup <- list()
+  pb_setup$re <- "\\r([0-9]+) +[A-Z]+ +(?:[0-9]+ of )*([0-9]+) +(?:[0-9\\.]+|-+) +([0-9\\.]+|-+) +[0-9:]+ +[0-9,]+"
+  pb_setup$score <- 3L
+  pb_setup$ratio_num <- 1L
+  pb_setup$hits <- 1L
+  if (class(obj@tree_search) == "NitroBranchSwap") {
+    pb_setup$total <- replications(obj@tree_search)
+    pb_setup$format <- "Branch swapping: [:bar] :current/:total reps | Best score: :score"
+  } else if (class(obj@tree_search) == "NitroRatchet") {
+    pb_setup$total <- iterations(obj@tree_search)
+    pb_setup$format <- "Ratcheting: [:bar] :current/:total iters | Best score: :score"
+  } else if (class(obj@tree_search) == "NitroDriven") {
+    pb_setup$hits <- hits(obj@tree_search)
+    pb_setup$ratio_num <- 2L
+    pb_setup$total <- replications(obj@tree_search)
+    pb_setup$format <- paste0("Driven search: [:bar]", ifelse(hits(obj@tree_search) > 1, " run :run", ""), " | :current/:total reps | Best score: :score")
+  } else if (class(obj@tree_search) == "NitroImplicitEnum") {
+    pb_setup$re <- "Searching \\(score ([0-9]+)\\) ([X=]+)\r"
+    pb_setup$score <- 1L
+    pb_setup$total <- 30L
+    pb_setup$format <- "Implicit enumeration: [:bar] | Best score :score"
+  }
+
+  pb <- progress_bar$new(format = pb_setup$format, total = pb_setup$total)
+  pb$tick(0)
 
   # Define callback function for process line-by-line TNT output
   callback <- function (out, proc) {
-    prog.info <- params$progress$value(out)
-    if (!is.null(prog.info) & !params$progress$bar$finished) {
-      params$progress$bar$update(prog.info$ratio, tokens = prog.info$tokens)
+    prog_re <- regexec(pb_setup$re, out)
+    if(length(attr(prog_re[[1]], "match.length")) != 1) {
+      do_update = TRUE
+      prog_m <- tail(regmatches(out, prog_re)[[1]], -1)
+      score <- prog_m[pb_setup$score]
+      tnt_prog_re <- regexec("(X*)=+", prog_m[pb_setup$ratio_num])[[1]]
+      if (attr(tnt_prog_re, "match.length")[1] > -1) {
+        ratio_num <- attr(tnt_prog_re, "match.length")[2]
+      } else {
+        ratio_num <- as.numeric(prog_m[pb_setup$ratio_num])
+      }
+      ratio <- ratio_num / pb_setup$total
+      if (ratio > 1) {
+        do_update <- FALSE
+      }
+      tokens <- list(score = score)
+      if (pb_setup$hits > 1) {
+        tokens$run <- as.numeric(prog_m[1]) + 1
+      }
+      if (do_update & !pb$finished) {
+        pb$update(ratio, tokens = tokens)
+      }
     }
     return(out)
   }
 
   # Run TNT binary and collect output
-  tnt.output <- run(normalizePath(tnt.path), tnt.arg,
+  tnt_output <- run(normalizePath(tnt_path), tnt_arg,
                     stderr_callback = callback)
 
   # Terminate progress bar and split TNT stdout
-  if (!params$progress$bar$finished) {
-    params$progress$terminate
+  if (!pb$finished) {
+    pb$update(1)
+    pb$terminate
   }
 
-  # Check for any error messages and display if found
-  err.out <- strsplit(tnt.output$stderr, "[\n\r]+")[[1]]
-  err.re <- sapply(regmatches(err.out, regexec("^Error reading ", err.out)),
+  err_out <- strsplit(tnt_output$stderr, "[\n\r]+")[[1]]
+
+  # Check if tree buffer filled during analysis
+  buf_re <- sapply(regmatches(err_out, regexec("(?:tree buffer full|Increase max.trees)", err_out)),
                    length)
-  if (any(err.re)) {
-    stop(sub("^\a", "", err.out[which(err.re > 0)[1] - 1]))
+  if (any(buf_re)) {
+    stop("Tree buffer filled during analysis; consider increasing 'hold' value")
   }
 
-  tnt.output <- strsplit(tnt.output$stdout, "[\n\r]+")[[1]]
-  params$trees <- tntTreeParse(tnt.output, names(params$matrix))
-  params$progress <- NULL
-  return(params)
+  # Check if insufficient RAM for tree buffer size
+  ram_re <- sapply(regmatches(err_out, regexec("Cannot make tree space", err_out)), length)
+  if (any(ram_re)) {
+    stop(paste("Tree buffer cannot hold", as.integer(hold),
+               "trees; consider increasing 'max_ram' value"))
+  }
+
+  # Check for overflowed replications in branch swap analyses
+  overflow_re <- sapply(regmatches(err_out, regexec("some replications overflowed", err_out)),
+                        length)
+  if (any(overflow_re)) {
+    message("Warning: Some replications overflowed")
+  }
+
+  # Check for other error messages and display if found
+  err_re <- sapply(regmatches(err_out, regexec("^Error reading ", err_out)),
+                   length)
+  if (any(err_re)) {
+    stop(sub("^\a", "", err_out[which(err_re > 0)[1] - 1]))
+  }
+
+  tnt_output <- strsplit(tnt_output$stdout, "[\n\r]+")[[1]]
+  trees <- tnt_output_parse(tnt_output, rownames(tnt_matrix))
+  return(trees)
 }
--- R/tntTreeParse.R
+++ R/tntTreeParse.R
@@ -6,20 +6,20 @@ if(getRversion() >= "2.15.1") {
 #'
 #' @importFrom magrittr %>%
 #' @importFrom ape read.tree .compressTipLabel
-#' @param tnt.output a character vector containing raw TNT output from the
+#' @param tnt_output a character vector containing raw TNT output from the
 #'   \code{implicit.enum}, \code{branchswap}, \code{ratchet} or \code{driven}
 #'   functions.
-#' @param tip.labels a list or vector containing the names of the taxa from the
+#' @param tip_labels a list or vector containing the names of the taxa from the
 #'   analysed matrix.
 #' @return a \code{multiPhylo} object.
-tntTreeParse <- function (tnt.output, tip.labels) {
+tnt_output_parse <- function (tnt_output, tip_labels) {
   # Get start lines for tree strings and lengths output
-  first.trees <- grep("Tread 'set of [0-9]+ trees' ", tnt.output) + 1
-  tree.lengths <- grep("Tree lengths ", tnt.output)
-  weighted.scores <- grep("Adjusted homoplasy ", tnt.output)
+  first_trees <- grep("Tread 'set of [0-9]+ trees' ", tnt_output) + 1
+  tree_lengths <- grep("Tree lengths ", tnt_output)
+  weighted_scores <- grep("Adjusted homoplasy ", tnt_output)
 
   # Convert TNT's tree strings into newick format
-  newick.trees <- paste(tnt.output[first.trees:(tree.lengths - 2)],
+  newick_trees <- paste(tnt_output[first_trees:(tree_lengths - 2)],
                         collapse = "") %>%
     gsub(" [*;] +", ";*", .) %>%
     strsplit("\\*") %>%
@@ -29,14 +29,14 @@ tntTreeParse <- function (tnt.output, tip.labels) {
     gsub("\\)\\(","),(", .)
 
   # Get total min and max steps
-  minsteps.match <- grep("Minimum possible steps \\(total = ([0-9]+)\\) ",
-                         tnt.output)
-  maxsteps.match <- grep("Maximum possible steps \\(total = ([0-9]+)\\) ",
-                         tnt.output)
+  minsteps_match <- grep("Minimum possible steps \\(total = ([0-9]+)\\) ",
+                         tnt_output)
+  maxsteps_match <- grep("Maximum possible steps \\(total = ([0-9]+)\\) ",
+                         tnt_output)
   minsteps <- as.numeric(gsub(".* ([0-9]+)\\) ", "\\1",
-                              tnt.output[minsteps.match]))
+                              tnt_output[minsteps_match]))
   maxsteps <- as.numeric(gsub(".* ([0-9]+)\\) ", "\\1",
-                              tnt.output[maxsteps.match]))
+                              tnt_output[maxsteps_match]))
 
   tableParse <- function (table) {
     return(table[c(TRUE, rep(FALSE, 2))] %>%
@@ -47,23 +47,23 @@ tntTreeParse <- function (tnt.output, tip.labels) {
   }
 
   # Get all tree lengths
-  lengths <- tableParse(tnt.output[(tree.lengths+3):(minsteps.match-3)])
+  lengths <- tableParse(tnt_output[(tree_lengths+3):(minsteps_match-3)])
 
-  tree.attrs <- cbind(newick.trees, lengths)
+  tree_attrs <- cbind(newick_trees, lengths)
 
   scores <- NULL
-  if (length(weighted.scores)) {
-    scores <- tableParse(tnt.output[(weighted.scores+3):length(tnt.output)])
-    tree.attrs <- cbind(tree.attrs, scores)
+  if (length(weighted_scores)) {
+    scores <- tableParse(tnt_output[(weighted_scores+3):length(tnt_output)])
+    tree_attrs <- cbind(tree_attrs, scores)
   }
 
-    # Create phylo objects from newick trees; calculate statistics
-  trees <- apply(tree.attrs, 1, function (tree.data) {
-    tree <- read.tree(text=tree.data[1])
-    tree$tip.label <- tip.labels[as.numeric(tree$tip.label) + 1]
-    tree$length <- as.numeric(tree.data[2])
-    if (length(tree.data) > 2) {
-      tree$weighted.score <- as.numeric(tree.data[3])
+  # Create phylo objects from newick trees; calculate statistics
+  trees <- apply(tree_attrs, 1, function (tree_data) {
+    tree <- read.tree(text=tree_data[1])
+    tree$tip.label <- tip_labels[as.numeric(tree$tip.label) + 1]
+    tree$length <- as.numeric(tree_data[2])
+    if (length(tree_data) > 2) {
+      tree$weighted_score <- as.numeric(tree_data[3])
     }
     tree$CI <- minsteps / tree$length
     tree$RI <- (maxsteps - tree$length) / (maxsteps - minsteps)
