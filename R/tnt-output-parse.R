12d1691bcbe9cd2f33260f850914242a574dc780
paravian <contact@paravian.net>
2020-07-27 14:50:25 +1000

    Revise output parsing; save tree stats as attributes

--- R/tnt-output-parse.R
+++ R/tnt-output-parse.R
@@ -6,6 +6,7 @@ if(getRversion() >= "2.15.1") {
 #'
 #' @importFrom magrittr %>%
 #' @importFrom ape read.tree .compressTipLabel
+#' @importFrom TreeTools TNTText2Tree
 #' @param tnt_output a character vector containing raw TNT output from the
 #'   \code{implicit.enum}, \code{branchswap}, \code{ratchet} or \code{driven}
 #'   functions.
@@ -14,19 +15,17 @@ if(getRversion() >= "2.15.1") {
 #' @return a \code{multiPhylo} object.
 tnt_output_parse <- function (tnt_output, tip_labels) {
   # Get start lines for tree strings and lengths output
-  first_trees <- grep("Tread 'set of [0-9]+ trees' ", tnt_output) + 1
-  tree_lengths <- grep("Tree lengths ", tnt_output)
-  weighted_scores <- grep("Adjusted homoplasy ", tnt_output)
+  trees_match <- grep("Tread 'set of [0-9]+ trees' ", tnt_output) + 1
+  lengths_match <- grep("Tree lengths ", tnt_output)
+  scores_match <- grep("Adjusted homoplasy ", tnt_output)
 
-  # Convert TNT's tree strings into newick format
-  newick_trees <- paste(tnt_output[first_trees:(tree_lengths - 2)],
-                        collapse = "") %>%
+  # Split TNT's tree strings
+  tree_attrs <-
+    data.frame(trees = paste(tnt_output[trees_match:(lengths_match - 2)],
+                             collapse = "") %>%
     gsub(" [*;] +", ";*", .) %>%
     strsplit("\\*") %>%
-    unlist %>%
-    gsub(" ", ",", .) %>%
-    gsub(",\\)", ")", .) %>%
-    gsub("\\)\\(","),(", .)
+    unlist)
 
   # Get total min and max steps
   minsteps_match <- grep("Minimum possible steps \\(total = ([0-9]+)\\) ",
@@ -47,27 +46,32 @@ tnt_output_parse <- function (tnt_output, tip_labels) {
   }
 
   # Get all tree lengths
-  lengths <- tableParse(tnt_output[(tree_lengths+3):(minsteps_match-3)])
+  tree_attrs$lengths <- as.numeric(tableParse(
+    tnt_output[(lengths_match+3):(minsteps_match-3)]))
 
-  tree_attrs <- cbind(newick_trees, lengths)
 
   scores <- NULL
-  if (length(weighted_scores)) {
-    scores <- tableParse(tnt_output[(weighted_scores+3):length(tnt_output)])
-    tree_attrs <- cbind(tree_attrs, scores)
+  if (length(scores_match)) {
+    tree_attrs$scores <- as.numeric(tableParse(
+      tnt_output[(scores_match+3):length(tnt_output)]))
   }
 
   # Create phylo objects from newick trees; calculate statistics
-  trees <- apply(tree_attrs, 1, function (tree_data) {
-    tree <- read.tree(text=tree_data[1])
+  sum_minsteps <- sum(minsteps)
+  sum_maxsteps <- sum(maxsteps)
+  trees <- lapply(1:nrow(tree_attrs), function (tree_row) {
+    tree_data <- tree_attrs[tree_row,]
+    tree <- TNTText2Tree(tree_data["trees"])
     tree$tip.label <- tip_labels[as.numeric(tree$tip.label) + 1]
-    tree$length <- as.numeric(tree_data[2])
-    if (length(tree_data) > 2) {
-      tree$weighted_score <- as.numeric(tree_data[3])
+    attr(tree, "length") <- tree_data["lengths"]
+    if (length(scores_match)) {
+      attr(tree, "weighted_score") <- as.numeric(tree_data["scores"])
     }
-    tree$CI <- minsteps / tree$length
-    tree$RI <- (maxsteps - tree$length) / (maxsteps - minsteps)
-    tree$RC <- tree$CI * tree$RI
+    attr(tree, "CI") <- sum_minsteps / tree_data["lengths"]
+    attr(tree, "RI") <- (sum_maxsteps - tree_data["lengths"]) /
+      (sum_maxsteps - sum_minsteps)
+    attr(tree, "RC") <- attr(tree, "CI") * attr(tree, "RI")
+    }
     return(tree)
   })
   return(.compressTipLabel(trees))
