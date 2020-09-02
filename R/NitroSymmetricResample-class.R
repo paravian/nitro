0fc32b923328051c80617d22452eb8237466563c
paravian <contact@paravian.net>
2020-09-02 16:25:28 +1000

    Classes and methods for symmetric resampling

---
+++ R/NitroSymmetricResample-class.R
@@ -0,0 +1,19 @@
+#' Define symmetric resampling analysis
+#'
+#' \code{NitroSymmetricResample} is an S4 class that defines parameters for
+#' symmetric resampling analyses.
+#' @name NitroSymmetricResample-class
+#' @docType class
+#' @seealso The inheriting S4 class
+#' \code{"\linkS4class{NitroConstraintsBase}"}.
+#' @keywords classes
+#' @include check-classes.R
+#' @include NitroResampleBase-class.R
+setClass("NitroSymmetricResample",
+  contains = c("NitroResampleBase"),
+  slots = c(
+    probability = "integer",
+    cutoff = "numeric"
+))
+
+setValidity("NitroSymmetricResample", check_NitroJacknife)
