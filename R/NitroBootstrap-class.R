e63aacc92df09f5b4f0a949513dcd6c683c928fb
Tom Brougham <tbrougham@paravian.net>
2020-11-20 15:39:18 +1100

    New R6 class for bootstrapping

--- R/NitroBootstrap-class.R
+++ R/NitroBootstrap-class.R
@@ -1,18 +1,56 @@
-#' Define bootstrap resampling analysis
+#' Define a bootstrap resampling analysis
 #'
-#' \code{NitroBootstrap} is an S4 class that defines parameters for
+#' @description
+#' \code{NitroBootstrap} is an R6 class that defines parameters for
 #' bootstrap resampling analyses.
-#' @name NitroBootstrap-class
-#' @docType class
-#' @seealso The inheriting S4 class
-#' \code{"\linkS4class{NitroConstraintsBase}"}.
-#' @keywords classes
-#' @include check-classes.R
-#' @include NitroResampleBase-class.R
-setClass("NitroBootstrap",
-  contains = c("NitroResampleBase"),
-  slots = c(
-    cutoff = "numeric"
-))
-
-setValidity("NitroBootstrap", check_NitroBootstrap)
+#' @importFrom R6 R6Class
+#' @importFrom checkmate assertInt
+#' @export
+NitroBootstrap <- R6Class("NitroBootstrap",
+  inherit = NitroResampleBase,
+  private = list(
+    .cutoff = NULL
+  ),
+  #' @field cutoff An integer value indicating the cutoff value for
+  #'   frequencies.
+  active = list(
+    cutoff = function (value) {
+      if (missing(value)) {
+        private$.cutoff
+      } else {
+        assertInt(value, lower = 0, upper = 99)
+        private$.cutoff <- asInt(value)
+        self
+      }
+    }),
+  public = list(
+    #' @param cutoff An integer value indicating the cutoff value for
+    #'   frequencies.
+    #' @param search_method An object inheriting class
+    #'   \code{"\link{NitroMethodsBase}"}.
+    #' @param phy A tree of class \code{phylo}. Resampling values will be
+    #'   calculated using this topology.
+    #' @param replications An integer value indicating the number of resampling
+    #'   replications to perform.
+    initialize = function (cutoff = 0, search_method = NULL, phy = NULL,
+                           replications = 100) {
+      a <- as.list(environment(), all = TRUE)
+      for (n in names(a)) {
+        self[[n]] <- a[[n]]
+      }
+    },
+    #' @param ... Ignored.
+    print = function (...) {
+      cat("<NitroBootstrap>\n")
+      cat(paste("* Replications:", private$.replications, "\n"))
+      cat(paste("* Frequency cutoff:", private$.cutoff, "\n"))
+    },
+    #' @param ... Ignored.
+    tnt_cmd = function (...) {
+      paste("resample= boot from 0 replications ", private$.replications,
+            " cut ", private$.cutoff,
+            " gc frequency [ ", paste(private$.search_method$tnt_cmd(), collapse = " "),
+            " ];", sep = "")
+    }
+  )
+)
