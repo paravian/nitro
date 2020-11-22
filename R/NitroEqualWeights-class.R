60504e5d8871a0f4361c8db14ca6dc47d7290033
paravian <contact@paravian.net>
2020-11-22 14:21:24 +1100

    New R6 class for equal weighting

--- R/NitroEqualWeights-class.R
+++ R/NitroEqualWeights-class.R
@@ -1,13 +1,16 @@
 #' Define equal weights analysis
 #'
-#' \code{NitroEqualWeights} is an S4 class that serves as the basis for classes
+#' @description
+#' \code{NitroEqualWeights} is an R6 class that serves as the basis for classes
 #' that define parameters for equal weights phylogenetic analyses.
-#' @name NitroEqualWeights-class
-#' @docType class
-#' @seealso The inheriting S4 class \code{"\linkS4class{NitroWeightsBase}"}
-#' and \code{"\linkS4class{NitroImpliedWeights}"}.
-#' @keywords classes
-#' @include NitroWeightsBase-class.R
-setClass("NitroEqualWeights",
-  contains = "NitroWeightsBase"
+#' @importFrom R6 R6Class
+#' @export
+NitroEqualWeights <- R6Class("NitroEqualWeights",
+  inherit = NitroWeightsBase,
+  public = list(
+    #' @param ... Ignored.
+    print = function (...) {
+      cat("<NitroEqualWeights>")
+    }
+  )
 )
