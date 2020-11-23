7b25c390475fccb7081046e82e696160253a9d2e
Tom Brougham <tbrougham@paravian.net>
2020-11-23 13:43:26 +1100

    New R6 base class for weighting

--- R/NitroWeightsBase-class.R
+++ R/NitroWeightsBase-class.R
@@ -1,11 +1,8 @@
 #' Define tree searches
 #'
-#' \code{NitroWeighBase} is an S4 class that serves as the basis for classes
+#' @description
+#' \code{NitroWeighBase} is an R6 class that serves as the basis for classes
 #' that define parameters for equal and implied weights analyses.
-#' @name NitroWeightsBase-class
-#' @docType class
-#' @seealso The inheriting S4 classes \code{"\linkS4class{NitroEqualWeights}"}
-#' and \code{"\linkS4class{NitroImpliedWeights}"}.
-#' @keywords classes
-#' @include check-classes.R
-setClass("NitroWeightsBase", contains = "VIRTUAL")
+#' @importFrom R6 R6Class
+#' @export
+NitroWeightsBase <- R6Class("NitroWeightsBase")
