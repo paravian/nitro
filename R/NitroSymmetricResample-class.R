bc8de33f22185a897b30bf541aafa2b2f72a4dff
paravian <contact@paravian.net>
2020-11-23 13:35:29 +1100

    New R6 class for symmetric resampling

--- R/NitroSymmetricResample-class.R
+++ R/NitroSymmetricResample-class.R
@@ -1,19 +1,69 @@
 #' Define symmetric resampling analysis
 #'
-#' \code{NitroSymmetricResample} is an S4 class that defines parameters for
+#' @description
+#' \code{NitroSymmetricResample} is an R6 class that defines parameters for
 #' symmetric resampling analyses.
-#' @name NitroSymmetricResample-class
-#' @docType class
-#' @seealso The inheriting S4 class
-#' \code{"\linkS4class{NitroConstraintsBase}"}.
-#' @keywords classes
-#' @include check-classes.R
-#' @include NitroResampleBase-class.R
-setClass("NitroSymmetricResample",
-  contains = c("NitroResampleBase"),
-  slots = c(
-    probability = "integer",
-    cutoff = "numeric"
-))
-
-setValidity("NitroSymmetricResample", check_NitroJackknife)
+#' @importFrom checkmate asInt assertInt
+#' @importFrom R6 R6Class
+#' @export
+NitroSymmetricResample <- R6Class("NitroSymmetricResample",
+  inherit = NitroResampleBase,
+  private = list(
+    .probability = NULL,
+    .cutoff = NULL
+  ),
+  active = list(
+    #' @field probability An integer value indicating the change probability.
+    probability = function (value) {
+      if (missing(value)) {
+        private$.probability
+      } else {
+        assertInt(value, lower = 0, upper = 99)
+        private$.probability <- asInt(value)
+      }
+    },
+    #' @field cutoff An integer value indicating the cutoff value for
+    #'   frequencies.
+    cutoff = function (value) {
+      if (missing(value)) {
+        private$.cutoff
+      } else {
+        assertInt(value, lower = 0, upper = 99)
+        private$.cutoff <- asInt(value)
+      }
+    }
+  ),
+  public = list(
+    #' @param probability An integer value indicating the change probability.
+    #' @param cutoff An integer value indicating the cutoff value for
+    #'   frequencies.
+    #' @param search_method An object inheriting class
+    #'   \code{"\link{NitroMethodsBase}"}.
+    #' @param phy A tree of class \code{phylo}. Resampling values will be
+    #'   calculated using this topology.
+    #' @param replications an integer value indicating the number of resampling
+    #'   replications to perform.
+    initialize = function (probability = 36, cutoff = 0, search_method = NULL,
+                           phy = NULL, replications = 100) {
+      a <- as.list(environment(), all = TRUE)
+      for (n in names(a)) {
+        self[[n]] <- a[[n]]
+      }
+    },
+    #' @param ... Ignored.
+    print = function (...) {
+      cat("<NitroSymmetricResample>\n")
+      cat(paste("* Removal probability:", private$.probability, "\n"))
+      cat(paste("* Frequency cutoff:", private$.cutoff, "\n"))
+      cat(paste("* Replications:", private$.replications, "\n"))
+    },
+    #' @param ... Ignored.
+    tnt_cmd = function (...) {
+      paste("resample= sym from 0 replications ", private$.replications,
+            " probability ", private$.probability,
+            " cut ", private$.cutoff,
+            " gc frequency [ ", paste(private$.search_method$tnt_cmd(), collapse = " "),
+            " ];", sep = "")
+    }
+  )
+)
