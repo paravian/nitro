fc6cf98bd21bd23989ba20b05881ebd48843d7b7
paravian <contact@paravian.net>
2020-11-23 13:55:38 +1100

    Update TNT nexus file writer

--- R/writeTNTNexus.R
+++ R/writeTNTNexus.R
@@ -1,9 +1,10 @@
 #' Write a TNT-compatible nexus file
 #'
-#' Function to write a nexus file from a \code{"\linkS4class{NitroTreeSearch}"}
+#' Function to write a nexus file from a \code{"\link{NitroTreeSearch}"}
 #' object, containing a phylogenetic matrix and TNT tree search commands
 #' required as required for an analysis in \code{nitro}. This is used
 #' internally by \code{\link{tnt}}.
+#' @importFrom ape write.nexus.data
 #' @details The function goes through the following stages, in order:
 #'
 #' \enumerate{
@@ -39,24 +40,24 @@
 #'   consistency, retention and rescaled consistency indices (CI, RI, RC) are
 #'   calculated from the output. These values, together with the tree
 #'   lengths/scores, are saved with each \code{phylo} tree.
-#' \item A \code{"\linkS4class{NitroTrees}"} object is returned which contains
+#' \item A \code{"\link{NitroTrees}"} object is returned which contains
 #'   a \code{multiPhylo} object in the \code{trees} slot and the original
-#'   \code{"\linkS4class{NitroTreeSearch}"} object in the \code{tree_search}
+#'   \code{"\link{NitroTreeSearch}"} object in the \code{tree_search}
 #'   slot.
 #' }
-#' @param obj an object that inherits \code{"\linkS4class{NitroTreeSearch}"}.
-#' @param filename a character vector indicating the name for the new file.
-#' @param read_trees a logical value indicating whether to read in trees from
+#' @param obj An object that inherits \code{"\link{NitroTreeSearch}"}.
+#' @param filename A character vector indicating the name for the new file.
+#' @param read_trees A logical value indicating whether to read in trees from
 #'   the \code{start_trees} slot of \code{obj} prior to analysis.
-#' @param character_fits a logical value indicating whether to return scores
+#' @param character_fits A logical value indicating whether to return scores
 #'   for individual characters and minimum and maximum possible character
 #'   lengths required for calculation of character fit indices.
-#' @rdname writeTNTNexus
+#' @rdname write_tnt_nexus
 #' @export
-writeTNTNexus <- function (obj, filename, read_trees = FALSE,
+write_tnt_nexus <- function (obj, filename, read_trees = FALSE,
                            character_fits = FALSE) {
   # Read matrix; convert NAs to "?" and write to minimal nexus file
-  tnt_matrix <- obj@matrix
+  tnt_matrix <- obj$matrix
   tnt_matrix[is.na(tnt_matrix)] <- "?"
   write.nexus.data(tnt_matrix, file=filename, interleaved = FALSE,
                    format = "standard")
@@ -64,72 +65,75 @@ writeTNTNexus <- function (obj, filename, read_trees = FALSE,
 
   tnt_block <- c("BEGIN TNT;", "log stdout;", "tables =;")
 
-  if (inherits(obj@weights, "NitroImpliedWeights")) {
-    iw_opts <- tnt_cmd(obj@weights)
-    if (obj@weights@multi_k) {
+  if (inherits(obj$weights, "NitroImpliedWeights")) {
+    iw_opts <- obj$weights$tnt_cmd()
+    if (obj$weights$multi_k) {
       tnt_block <- c(tnt_block, iw_opts[2])
     }
   }
 
-  if (inherits(obj@method, c("NitroRatchet", "NitroResampleBase"))) {
+  if (inherits(obj$method, c("NitroRatchet", "NitroResampleBase"))) {
     if (!read_trees) {
       read_trees <- TRUE
     }
-    if (inherits(obj@method, "NitroResampleBase")) {
-      obj@start_trees <- c(obj@method@phy)
+    if (inherits(obj$method, "NitroResampleBase")) {
+      obj$start_trees <- obj$method$phy
     }
   }
 
   tnt_block <- c(tnt_block,
-                 paste0("collapse ", collapse(obj), ";"),
-                 paste0("hold ", as.integer(obj@hold), ";"),
-                 paste0("outgroup ", obj@outgroup - 1, ";")
+                 paste0("collapse ", obj$collapse, ";"),
+                 paste0("hold ", as.integer(obj$hold), ";"),
+                 paste0("outgroup ", obj$outgroup - 1, ";")
   )
 
   char_codes <- c()
-  if (any(obj@ordered_characters)) {
+  if (any(obj$ordered_characters)) {
     char_codes <- c(char_codes, "+",
-                    which(obj@ordered_characters) - 1)
+                    which(obj$ordered_characters) - 1)
   }
-  if (any(obj@inactive_characters)) {
+  if (any(obj$inactive_characters)) {
     char_codes <- c(char_codes, "]",
-                    which(obj@inactive_characters) - 1)
+                    which(obj$inactive_characters) - 1)
   }
   if (length(char_codes)) {
     tnt_block <- c(tnt_block, paste(c("ccode", char_codes, ";"), collapse=" "))
   }
-  if (any(obj@inactive_taxa)) {
+  if (any(obj$inactive_taxa)) {
     tnt_block <- c(tnt_block,
                    paste(c("taxcode -",
-                           which(obj@inactive_taxa) - 1, ";"), collapse=" "))
+                           which(obj$inactive_taxa) - 1, ";"), collapse=" "))
   }
 
-  if (length(obj@constraints)) {
-    tnt_block <- c(tnt_block, tnt_cmd(obj@constraints))
+  if (length(obj$constraints)) {
+    constraints <- paste(c("force ",
+                           sapply(obj$constraints, function (c) c$tnt_cmd()), ";"),
+                         collapse = "")
+    tnt_block <- c(tnt_block, constraints, "constrain =;")
   }
 
   if (read_trees) {
-    if (!length(obj@start_trees)) {
-      if (inherits(obj@method, "NitroRatchet")) {
+    if (!length(obj$start_trees)) {
+      if (inherits(obj$method, "NitroRatchet")) {
         tnt_block <- c(tnt_block, "mult= wagner replic 10;")
       } else {
         stop("start_trees contains no trees")
       }
     }
-    res <- new("NitroTrees", tree_search = obj, trees = obj@start_trees)
-    tnt_block <- c(tnt_block, tnt_cmd(res))
+    res <- NitroTrees$new(tree_search = obj, trees = obj$start_trees)
+    tnt_block <- c(tnt_block, res$tnt_cmd())
   }
 
-  if (inherits(obj@method, "NitroResampleBase")) {
+  if (inherits(obj$method, "NitroResampleBase")) {
     rstree_file <- sub("(\\..+|$)", ".tre", filename)
-    tnt_block <- c(tnt_block, "ttags =;", tnt_cmd(obj@method), "ttags );",
+    tnt_block <- c(tnt_block, "ttags =;", obj$method$tnt_cmd(), "ttags );",
                    paste("tsave *", rstree_file, sep = ""), "save *;",
                    "tsave /;")
   } else {
-    tnt_block <- c(tnt_block, tnt_cmd(obj@method), "condense;")
-    if (obj@combine) {
-      res <- new("NitroTrees", tree_search = obj, trees = obj@start_trees)
-      tnt_block <- c(tnt_block, tnt_cmd(res), "unique;")
+    tnt_block <- c(tnt_block, obj$method$tnt_cmd(), "condense;")
+    if (obj$combine) {
+      res <- NitroTrees$new(tree_search = obj, trees = obj$start_trees)
+      tnt_block <- c(tnt_block, res$tnt_cmd(), "unique;")
     }
   }
 
