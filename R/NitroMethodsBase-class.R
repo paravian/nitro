e8bacf5fad9d2e2f8edb9b6ff6ad065d490ff34e
paravian <contact@paravian.net>
2020-11-23 12:12:10 +1100

    New R6 base class for phylo methods

--- R/NitroMethodsBase-class.R
+++ R/NitroMethodsBase-class.R
@@ -1,13 +1,8 @@
 #' Define tree searches
 #'
-#' \code{NitroMethodsBase} is an S4 class that serves as the basis for classes that
-#' define parameters for tree search methods.
-#' @name NitroMethodsBase-class
-#' @docType class
-#' @seealso The inheriting S4 classes \code{"\linkS4class{NitroImplicitEnum}"},
-#' \code{"\linkS4class{NitroBranchSwap}"},
-#' \code{"\linkS4class{NitroRatchet}"} and
-#' \code{"\linkS4class{NitroDriven}"}.
-#' @keywords classes
-#' @include check-classes.R
-setClass("NitroMethodsBase", contains = "VIRTUAL")
+#' @description
+#' \code{NitroMethodsBase} is a base R6 class for defining other classes that
+#' represent parameters for tree search methods.
+#' @importFrom R6 R6Class
+#' @export
+NitroMethodsBase <- R6Class("NitroMethodsBase")
