f9ace4aea38e650dce02a472c5e2203010ce8472
paravian <contact@paravian.net>
2023-09-13 16:41:30 +1000

    Update symmetric resampling R6 class

--- R/NitroSymmetricResample-class.R
+++ R/NitroSymmetricResample-class.R
@@ -1,50 +1,50 @@
-#' Define symmetric resampling analysis
+#' Set options for symmetric resampling
 #'
 #' @description
-#' \code{NitroSymmetricResample} is an R6 class that defines parameters for
+#' \code{SymmetricResamplingOptions} is an R6 class that defines parameters for
 #' symmetric resampling analyses.
 #' @importFrom checkmate asInt assertInt
+#' @importFrom glue glue
 #' @importFrom R6 R6Class
 #' @export
-NitroSymmetricResample <- R6Class("NitroSymmetricResample",
-  inherit = NitroResampleBase,
+SymmetricResamplingOptions <- R6Class("SymmetricResamplingOptions",
+  inherit = ResampleBaseOptions,
   private = list(
-    .probability = NULL,
-    .cutoff = NULL
+    .probability = NULL
   ),
   active = list(
     #' @field probability An integer value indicating the change probability.
     probability = function (value) {
       if (missing(value)) {
-        private$.probability
+        return(private$.probability)
       } else {
-        assertInt(value, lower = 0, upper = 99)
+        val_check <- check_int(value, lower = 0, upper = 99)
+        if (!isTRUE(val_check)) {
+          cli_abort(c("{.arg probability} must be an integer.",
+                      "x" = val_check))
+        }
         private$.probability <- asInt(value)
-      }
-    },
-    #' @field cutoff An integer value indicating the cutoff value for
-    #'   frequencies.
-    cutoff = function (value) {
-      if (missing(value)) {
-        private$.cutoff
-      } else {
-        assertInt(value, lower = 0, upper = 99)
-        private$.cutoff <- asInt(value)
       }
     }
   ),
   public = list(
     #' @param probability An integer value indicating the change probability.
-    #' @param cutoff An integer value indicating the cutoff value for
-    #'   frequencies.
     #' @param search_method An object inheriting class
     #'   \code{"\link{NitroMethodsBase}"}.
-    #' @param phy A tree of class \code{phylo}. Resampling values will be
-    #'   calculated using this topology.
-    #' @param replications an integer value indicating the number of resampling
+    #' @param replications An integer value indicating the number of resampling
     #'   replications to perform.
-    initialize = function (probability = 36, cutoff = 0, search_method = NULL,
-                           phy = NULL, replications = 100) {
+    #' @param cutoff An integer value indicating the cutoff value for
+    #'   frequencies.
+    #' @param frequency_summary A character vector indicating which method(s) to
+    #'   use to summarize supports. More than one option can be specified. The
+    #'   options are:
+    #' \itemize{
+    #'   \item \code{absolute}: absolute frequencies, default;
+    #'   \item \code{difference}: frequency differences (i.e., group supported/contradicted);
+    #'   \item \code{slope}: frequency slopes.
+    #' }
+    initialize = function (probability = 36, search_method = NULL, replications = 100,
+                           cutoff = 0, frequency_summary = "absolute") {
       a <- as.list(environment(), all = TRUE)
       for (n in names(a)) {
         self[[n]] <- a[[n]]
@@ -52,18 +52,45 @@ NitroSymmetricResample <- R6Class("NitroSymmetricResample",
     },
     #' @param ... Ignored.
     print = function (...) {
-      cat("<NitroSymmetricResample>\n")
-      cat(paste("* Removal probability:", private$.probability, "\n"))
-      cat(paste("* Frequency cutoff:", private$.cutoff, "\n"))
-      cat(paste("* Replications:", private$.replications, "\n"))
+      cli_text("{col_grey(\"# A TNT symmetric resampling configuration\")}")
+
+      options <- c(
+        "Removal probability:" = self$probability,
+        "Replications:" = self$replications,
+        "Cutoff frequency:" = self$cutoff,
+        "Frequency summary:" = paste(self$frequency_summary, collapse = ", ")
+      ) %>%
+        data.frame()
+
+      names(options) <- NULL
+      print(options)
     },
     #' @param ... Ignored.
-    tnt_cmd = function (...) {
-      paste("resample= sym from 0 replications ", private$.replications,
-            " probability ", private$.probability,
-            " cut ", private$.cutoff,
-            " gc frequency [ ", paste(private$.search_method$tnt_cmd(), collapse = " "),
-            " ];", sep = "")
+    queue = function (...) {
+      queue <- CommandQueue$new()
+
+      choices <- c(gc = "difference", frequency = "absolute", slope = "slope")
+      freq_summ <- pmatch(self$frequency_summary, choices) %>%
+        na.omit() %>%
+        {names(choices)[.]} %>%
+        paste(collapse = " ")
+      if (!"difference" %in% self$frequency_summary) {
+        freq_summ <- glue("nogc {freq_summ}")
+      }
+
+      sym_args <- glue("sym from 0 replications {self$replications} probability {self$probability} cut {self$cutoff} {freq_summ} [")
+
+      sm_queue <- self$search_method$queue()
+      while(sm_queue$length() > 0) {
+        cmd <- sm_queue$read_next() %$%
+          paste(name, arguments, sep = " ", collapse = " ") %>%
+          glue(";")
+        sym_args <- c(sym_args, cmd)
+      }
+      sym_args <- c(sym_args, "]")
+
+      queue$add("resample", sym_args)
+      return(queue)
     }
   )
 )
