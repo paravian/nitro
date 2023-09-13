4c5e80d0fff23056cf8cfb3d6b2829cac9e77e8c
paravian <contact@paravian.net>
2023-09-13 16:37:17 +1000

    Update jackknife R6 class

--- R/NitroJackknife-class.R
+++ R/NitroJackknife-class.R
@@ -1,50 +1,50 @@
-#' Define jackknife resampling analysis
+#' Set options for jackknife resampling
 #'
 #' @description
-#' \code{NitroJackknife} is an R6 class that defines parameters for
-#' jackknife resampling analyses.
-#' @importFrom checkmate assertInt
+#' \code{JackknifeOptions} is an R6 class that defines options for jackknife
+#'   resampling analyses.
+#' @importFrom checkmate check_int
+#' @importFrom cli cli_abort cli_text
 #' @importFrom R6 R6Class
 #' @export
-NitroJackknife <- R6Class("NitroJackknife",
-  inherit = NitroResampleBase,
+JackknifeOptions <- R6Class("JackknifeOptions",
+  inherit = ResampleBaseOptions,
   private = list(
-    .probability = NULL,
-    .cutoff = NULL
+    .probability = NULL
   ),
   active = list(
     #' @field probability An integer value indicating the change probability.
     probability = function (value) {
       if (missing(value)) {
-        private$.probability
+        return(private$.probability)
       } else {
-        assertInt(value, lower = 0, upper = 99)
+        val_check <- check_int(value, lower = 0, upper = 99)
+        if (!isTRUE(val_check)) {
+          cli_abort(c("{.arg probability} must be an integer.",
+                      "x" = val_check))
+        }
         private$.probability <- asInt(value)
-      }
-    },
-    #' @field cutoff An integer value indicating the cutoff value for
-    #'   frequencies.
-    cutoff = function (value) {
-      if (missing(value)) {
-        private$.cutoff
-      } else {
-        assertInt(value, lower = 0, upper = 99)
-        private$.cutoff <- asInt(value)
       }
     }
   ),
   public = list(
     #' @param probability An integer value indicating the change probability.
-    #' @param cutoff An integer value indicating the cutoff value for
-    #'   frequencies.
     #' @param search_method An object inheriting class
     #'   \code{"\link{NitroMethodsBase}"}.
-    #' @param phy A tree of class \code{phylo}. Resampling values will be
-    #'   calculated using this topology.
     #' @param replications An integer value indicating the number of resampling
     #'   replications to perform.
-    initialize = function (probability = 36, cutoff = 0, search_method = NULL,
-                           phy = NULL, replications = 100) {
+    #' @param cutoff An integer value indicating the cutoff value for
+    #'   frequencies.
+    #' @param frequency_summary A character vector indicating which method(s) to
+    #'   use to summarize supports. More than one option can be specified. The
+    #'   options are:
+    #' \itemize{
+    #'   \item \code{absolute}: absolute frequencies, default;
+    #'   \item \code{difference}: frequency differences (i.e., group supported/contradicted);
+    #'   \item \code{slope}: frequency slopes.
+    #' }
+    initialize = function (probability = 36, search_method = NULL, replications = 100,
+                           cutoff = 0, frequency_summary = "absolute") {
       a <- as.list(environment(), all = TRUE)
       for (n in names(a)) {
         self[[n]] <- a[[n]]
@@ -52,18 +52,45 @@ NitroJackknife <- R6Class("NitroJackknife",
     },
     #' @param ... Ignored.
     print = function (...) {
-      cat("<NitroJackknife>\n")
-      cat(paste("* Removal probability:", private$.probability, "\n"))
-      cat(paste("* Frequency cutoff:", private$.cutoff, "\n"))
-      cat(paste("* Replications:", private$.replications, "\n"))
+      cli_text("{col_grey(\"# A jackknife resampling configuration\")}")
+
+      options <- c(
+        "Removal probability:" = self$probability,
+        "Replications:" = self$replications,
+        "Cutoff frequency:" = self$cutoff,
+        "Frequency summary:" = paste(self$frequency_summary, collapse = ", ")
+      ) %>%
+        data.frame()
+
+      names(options) <- NULL
+      print(options)
     },
     #' @param ... Ignored.
-    tnt_cmd = function (...) {
-      paste("resample= jak from 0 replications ", private$.replications,
-            " probability ", private$.probability,
-            " cut ", private$.cutoff,
-            " gc frequency [ ", paste(private$.search_method$tnt_cmd(), collapse = " "),
-            " ];", sep = "")
+    queue = function (...) {
+      queue <- CommandQueue$new()
+
+      choices <- c(gc = "difference", frequency = "absolute", slope = "slope")
+      freq_summ <- pmatch(self$frequency_summary, choices) %>%
+        na.omit() %>%
+        {names(choices)[.]} %>%
+        paste(collapse = " ")
+      if (!"difference" %in% self$frequency_summary) {
+        freq_summ <- glue("nogc {freq_summ}")
+      }
+
+      jack_args <- glue("jak from 0 replications {self$replications} probability {self$probability} cut {self$cutoff} {freq_summ} [")
+
+      sm_queue <- self$search_method$queue()
+      while(sm_queue$length() > 0) {
+        cmd <- sm_queue$read_next() %$%
+          paste(name, arguments, sep = " ", collapse = " ") %>%
+          glue(";")
+        jack_args <- c(jack_args, cmd)
+      }
+      jack_args <- c(jack_args, "]")
+
+      queue$add("resample", jack_args)
+      return(queue)
     }
   )
 )
