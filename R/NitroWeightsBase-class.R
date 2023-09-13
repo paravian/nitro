fea93536e04e1ee2b2ac851a54a8665c038194c2
paravian <contact@paravian.net>
2023-09-13 16:35:00 +1000

    Remove weights base R6 class

--- R/NitroWeightsBase-class.R
+++
@@ -1,8 +0,0 @@
-#' Define tree searches
-#'
-#' @description
-#' \code{NitroWeighBase} is an R6 class that serves as the basis for classes
-#' that define parameters for equal and implied weights analyses.
-#' @importFrom R6 R6Class
-#' @export
-NitroWeightsBase <- R6Class("NitroWeightsBase")
