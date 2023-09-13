d4342d6ef81c67d37f597160bd4912a1418bf27a
paravian <contact@paravian.net>
2023-09-13 16:05:10 +1000

    Remove methods base R6 class

--- R/NitroMethodsBase-class.R
+++
@@ -1,8 +0,0 @@
-#' Define tree searches
-#'
-#' @description
-#' \code{NitroMethodsBase} is a base R6 class for defining other classes that
-#' represent parameters for tree search methods.
-#' @importFrom R6 R6Class
-#' @export
-NitroMethodsBase <- R6Class("NitroMethodsBase")
