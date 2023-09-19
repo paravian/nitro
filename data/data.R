24eb60b221bcc2d640c49f53ff63a1d98aac0bc2
paravian <contact@paravian.net>
2023-09-19 14:31:48 +1000

    Add Canale et al. (2022) dataset

---
+++ data/data.R
@@ -0,0 +1,10 @@
+#' Canale et al. (2022) matrix
+#' 
+#' A phylogenetic matrix of carcharodontisaurid theropod dinosaurs.
+#' 
+#' @format A phyDat object containing 20 taxa and 175 characters.
+#' @source Downloaded from the Supplemental Information section of
+#'   Canale et al. (2022) <https://doi.org/10.1016/j.cub.2022.05.057>,
+#'   converted into a Nexus file in Mesquite 3.61, and read with ReadAsPhydat
+#'   from TreeTools.
+"canale_2022"
