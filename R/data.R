3def5a11b80944697adceba047bc105c1228a86b
paravian <contact@paravian.net>
2023-09-29 14:48:55 +1000

    Add Raven and Maidment (2017) dataset

--- R/data.R
+++ R/data.R
@@ -8,3 +8,21 @@
 #'   converted into a Nexus file in Mesquite 3.61, and read with ReadAsPhydat
 #'   from TreeTools.
 "canale_2022"
+
+#' Raven and Maidment (2017) matrix
+#'
+#' A phylogenetic matrix of stegosaurian dinosaurs.
+#'
+#' @format A list containing the following objects:
+#'   \itemize{
+#'     \item \code{continuous}: A data frame containing 24 continuous character
+#'       traits; and
+#'     \item \code{discrete}: A phyDat object containing 91 characters.
+#'   }
+#'   Both objects score character data for the same 23 taxa.
+#' @source Downloaded from the Dryad Digital repository of Raven and
+#'   Maidment (2017) <https://doi.org/10.5061/dryad.ds543>. The TNT file was
+#'   converted into both a CSV file for the continuous data, and a Nexus file in
+#'   Mesquite 3.61 for the discrete character data. The CSV file was read with
+#'   `read.table` and the Nexus file was read with ReadAsPhydat from TreeTools.
+"raven_2017"
