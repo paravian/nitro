f0289e76134ddef211194da3b3fd5d37b31794db
Sienna Birch <tbroughm@une.edu.au>
2023-11-09 15:56:15 +1100

    Revised execute_queue with non-interactive processing

--- R/execute_queue.R
+++ R/execute_queue.R
@@ -4,12 +4,13 @@
 #' @importFrom cli cli_abort cli_progress_bar cli_progress_done
 #'   cli_progress_update pb_bar pb_eta_str pb_name pb_percent
 #' @importFrom stringr str_replace str_replace_all str_split str_trim str_wrap
-#' @param queue A \code{"\link{CommandQueue}"} object.
+#' @importFrom utils head tail
+#' @param tree_analysis A \code{"\link{TreeAnalysis}"} object.
 #' @param .envir The environment that TNT has been attached to.
-execute_queue <- function (queue, .envir) {
-  val_check <- check_class(queue, c("CommandQueue", "R6"))
+execute_queue <- function (tree_analysis, .envir) {
+  val_check <- check_class(tree_analysis, c("TreeAnalysis", "R6"))
   if (!test_true(val_check)) {
-    cli_abort(c("{.arg queue} must be a {.arg CommandQueue} object.",
+    cli_abort(c("{.arg tree_analysis} must be a {.arg TreeAnalysis} object.",
                 "x" = val_check))
   }
 
@@ -18,110 +19,109 @@ execute_queue <- function (queue, .envir) {
     cli_abort(c("{.arg .envir} must be an environment.",
                 "x" = val_check))
   }
-
-  tnt_process_start(.envir)
-  startup <- tnt_read(.envir)
-
-  tnt_info <- get("tnt_info", .envir)
-
-  n_cmds <- queue$length()
-  command <- queue$read_next()
-  all_output <- c()
-  res <- list()
-
-  # bar <- cli_progress_bar("Tree search")
-  cli_alert_info("Starting tree analysis...")
-  envir <- parent.frame()
+  
+  target <- list()
+  if (test_class(tree_analysis$method, "DrivenSearchOptions")) {
+    progress_re <- "(?<round>[0-9]+) +[A-Z]+ +(?<count>[0-9]+)"
+    target$rounds <- tree_analysis$method$hits
+  } else if (test_class(tree_analysis$method, "ResampleBaseOptions")) {
+    progress_re <- "[A-Za-z]+ \\(rep\\. (?<count>[0-9]+) of [0-9]+\\)"
+    target$count <- tree_analysis$method$replications
+  } else {
+    progress_re <- "(?<count>[0-9]+) +[A-Z]+ +[0-9]+ of [0-9]+"
+    if (test_class(tree_analysis$method, "RatchetOptions")) {
+      target$count <- tree_analysis$method$iterations
+    } else {
+      target$count <- tree_analysis$method$replications
+    }
+  }
+  
+  pb_envir <- parent.frame()
   cli_progress_bar(
     name = "Progress",
     total = 100,
-    .envir = envir
+    .envir = pb_envir
   )
 
-  status <- "parsing"
-  repl_input <- NULL
-  expected <- character()
-
-  while (status != "done") {
-    if (status == "parsing") {
-      command <- queue$read_next()
-
-      repl_input <- c(command$name, command$arguments)
-      if (length(repl_input) == 2) {
-        repl_input <- paste(repl_input, collapse = " ")
-      }
-      n_lines <- length(repl_input)
-
-      repl_input[n_lines] <- repl_input[n_lines] %>%
-        str_replace(";*$", "")
-      if (command$name == "xread") {
-        repl_input <- c(repl_input, ";")
-      } else {
-        repl_input[n_lines] <- paste(repl_input[n_lines], ";", sep = "")
-      }
-
-      repl_input <- str_wrap(repl_input, width = 254) %>%
-        str_split("\n") %>%
-        unlist()
-
-      if (any(nchar(repl_input) > 254)) {
-        repl_input <- str_replace_all(repl_input, "(.{254})", "\\1\n") %>%
-          str_split("\n") %>%
-          unlist()
-      }
-
-      expected <- c("tnt_prompt", command$name)
-      status <- "writing"
-    } else if (status == "writing") {
-      if (length(repl_input) > 0) {
-        tnt_info$process$write_input(glue("{repl_input[1]}\r"))
-        # cli_text("Writing: {repl_input[1]}")
-      }
-      status <- "waiting"
-    } else if (status == "waiting") {
-      output <- tnt_read(.envir)
-      # cli_text("Reading: ({output$eos_type}; {output$content_type}) {output$value}")
-
-      if (is.null(output)) {
-        cli_abort(c("No output received from TNT process.",
-                    "i" = "Attempted to execute {.arg {repl_input[1]}}"))
-      }
-
-      if (output$content_type == "error") {
-        tnt_error <- paste(output$value, collapse = " ") %>%
-          str_split("\n") %>%
-          unlist() %>%
-          str_trim()
-        names(tnt_error) <- rep("x", length(tnt_error))
-        cli_abort(c("Encountered an error while executing {.val {command$name}}",
-                    tnt_error))
-      } else if (output$content_type != "text") {
-        res[[output$content_type]] <- tnt_info$parser$transform(output$value, output$content_type)
-      }
-
-      if (output$eos_type %in% c("resample_progress", "search_progress")) {
-        frac <- tnt_info$parser$progress(command$name, command$arguments, output)
-        # cli_text("{frac}/100")
-        cli_progress_update(set = frac, .envir = envir)
-      } else if (output$eos_type %in% expected) {
-        if (output$eos_type == "tnt_prompt") {
-          if (queue$length() == 0) {
-            status <- "done"
-          } else {
-            status <- "parsing"
+  cli_alert_info("Starting tree analysis...")
+  
+  tnt_process_start(.envir, tree_analysis$queue())
+  
+  tnt_info <- get("tnt_info", .envir)
+  tnt_process <- tnt_info$process
+  
+  read_next <- TRUE
+  cmd_name <- "start"
+  buffer <- character()
+  cmd_re <- "COMMAND: (?<cmdname>[A-Z]+)"
+  
+  raw_out <- c()
+  
+  done <- FALSE
+  while (tnt_process$is_alive() | !done) {
+    proc_poll <- tnt_process$poll_io(5000)
+    
+    if (proc_poll[2] == "ready") {
+      proc_out <- tnt_process$read_error()
+      
+      # print(proc_out)
+      
+      has_cmd <- str_detect(proc_out, cmd_re)
+      has_counter <- str_detect(proc_out, progress_re)
+      
+      if (has_cmd) {
+        # Merge the buffer with the process output; split by command and write
+        # last command back to the buffer
+        proc_out <- paste(buffer, proc_out, sep = "")
+        cmd_out <- str_split_1(proc_out, cmd_re)
+        
+        raw_out <- c(raw_out, head(cmd_out, -1))
+        buffer <- tail(cmd_out, 1)
+        
+        buff_size <- nchar(str_trim(buffer))
+        if (!tnt_process$is_alive() & buff_size == 0) {
+          done <- TRUE
+        }
+      } else if (has_counter) {
+        progress <- str_match(proc_out, progress_re) %>%
+          extract(1,) %>%
+          extract(-1) %>%
+          as.list() %>%
+          lapply(as.numeric)
+        
+        if ("round" %in% names(progress)) {
+          denominator <- with(target, round * count)
+          numerator <- progress$round * target$count + (progress$count - 1)
+          if ((numerator / denominator) > 1) {
+            numerator <- (progress$round - 1) * target$count + (progress$count - 1)
           }
         } else {
-          repl_input <- repl_input[-1]
-          status <- "writing"
+          denominator <- target$count
+          numerator <- progress$count
         }
+        percent <- numerator / denominator * 100
+        cli_progress_update(set = percent, .envir = pb_envir)
       } else {
-        cli_abort(c("Unexpected output received from TNT. Cannot proceed.",
-                    "i" = "Output received: {.val {output$value}}"))
+        buffer <- paste(buffer, proc_out, sep = "")
       }
     }
   }
 
-  cli_progress_done(.envir = envir)
+  cli_progress_done(.envir = pb_envir)
   cli_alert_success("Tree analysis complete.")
-  return(res)
+ 
+  output_type <- sapply(raw_out, tnt_info$parser$content_detect)
+  
+  output <- list()
+  not_text <- output_type != "text"
+  if (any(not_text)) {
+    raw_out <- raw_out[not_text]
+    output_type <- output_type[not_text]
+    for (n in seq(sum(not_text))) {
+      obj <- tnt_info$parser$transform(raw_out[[n]], output_type[n])
+      output[[output_type[n]]] <- obj
+    }
+  }
+
+  return(output)
 }
