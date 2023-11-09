218df0f8945f019f2bc8f3bdf986076b3c9c2726
Sienna Birch <tbroughm@une.edu.au>
2023-11-09 15:53:08 +1100

    Revised tnt_attach with Windows support

--- R/tnt_attach.R
+++ R/tnt_attach.R
@@ -24,33 +24,72 @@ tnt_attach <- function (path, .envir = parent.frame()) {
                 "x" = val_check))
   }
 
+  platform <- .Platform$OS.type
+  
+  if ("tnt_info" %in% ls(envir = .envir)) {
+    try(tnt_info$process$kill(), silent = TRUE)
+    rm(tnt_info, envir = .envir)
+  }
+  
   check_tnt_info <- list(
-    platform = .Platform$OS.type,
-    path = normalizePath(path)
+    path = normalizePath(path),
+    platform = platform,
+    parser = OutputParser$new()
   )
-  check_tnt_info$parser <- OutputParser$new(check_tnt_info$platform)
+  assign("tnt_info", check_tnt_info, envir = .envir)
 
   # Prepare TNT for interactive use
-  assign("tnt_info", check_tnt_info, envir = .envir)
   tnt_process_start(.envir)
-  startup <- tnt_read(.envir)
   check_tnt_info <- get("tnt_info", envir = .envir)
   tnt_process <- check_tnt_info$process
 
+  prompts <- list(
+    yesno = "(press 'y' to accept, 'n' to decline)",
+    agree = "Do you agree to all the terms and conditions?",
+    done = "tnt\\*>|(enter \"help;\" for help)"
+  )
+  
+  
   ready <- FALSE
   pul_warned <- FALSE
   proc_read <- TRUE
+  buffer <- character()
   while (!ready) {
-    if (startup$eos_type == "licence") {
+    if (proc_read) {
+      proc_poll <- tnt_process$poll_io(5000)
+      
+      if (proc_poll[2] != "ready") {
+        next
+      }
+      
+      proc_out <- tnt_process$read_error() %>%
+        check_tnt_info$parser$clean()
+      
+      if (length(proc_out) == 0) {
+        next
+      }
+      
+      prompt_match <- sapply(prompts, str_detect, string = proc_out) %>%
+        apply(2, any) %>%
+        names(prompts)[.]
+    }
+    
+    
+    if ("yesno" %in% prompt_match) {
       if (!pul_warned) {
         cli_alert_warning("You must agree to TNT's licence before using {.pkg nitro}.")
+        if (platform == "windows") {
+          cli_abort(c("nitro can't interactively agree to TNT's licence terms on Windows.",
+                      "i" = "Please open {.val {check_tnt_info$path}} manually and agree to the licence terms, then run {.arg tnt_attach} again."))
+        }
         cli_alert_info("Please read and follow the instructions below.")
         pul_warned <- TRUE
       }
-
+      
       if (proc_read) {
-        cli_text(paste(startup$value, collapse = "\n"))
+        cli_text(proc_out)
       }
+      
       response <- readline(prompt = "Please enter [y]es or [n]o: ")
       valid_response <- check_choice(response, c("y", "n"))
       if (!test_true(valid_response)) {
@@ -58,43 +97,43 @@ tnt_attach <- function (path, .envir = parent.frame()) {
         proc_read <- FALSE
         next
       }
-      tnt_process$write_input(glue("{response}\r"))
-    } else if (startup$eos_type == "agree") {
+      tnt_process$write_input(response)
+    } else if ("agree" %in% prompt_match) {
       if (proc_read) {
-        cli_text(paste(startup$value, collapse = "\n"))
+        cli_text(proc_out)
       }
+      
       response <- readline(prompt = "Please enter \"I agree\": ")
       valid_response <- check_choice(response, c("I agree"))
+      
       if (!test_true(valid_response)) {
         cli_alert_danger("You must enter \"I agree\".")
         proc_read <- FALSE
         next
       }
-      tnt_process$write_input(glue("{response}\r"))
+      tnt_process$write_input(glue("{response}"))
       cli_alert_info("Please wait while TNT starts...")
-    } else if (startup$eos_type == "tnt_prompt") {
+    } else if ("done" %in% prompt_match) {
       ready <- TRUE
       break
     }
 
     proc_read <- TRUE
-
-    if (proc_read) {
-      startup <- tnt_read(.envir)
-      if (is.null(startup)) {
-        next
-      }
-    }
   }
 
-  version_re <- "T. N. T.  Version (?<number>[0-9]+\\.[0-9]+) - (?<bits>[0-9]+) bits - \\((?<date>[A-Za-z]+ [0-9]{4})\\)"
-  tnt_info <- str_extract(startup$value, version_re) %>%
+  if (platform == "windows") {
+    version_re <- "Version\\s+(?<number>[0-9]+\\.[0-9]+)\\s+(?<date>[A-Za-z]+ [0-9]{4})"
+  } else {
+    version_re <- "Version (?<number>[0-9]+\\.[0-9]+) - [0-9]+ bits - \\((?<date>[A-Za-z]+ [0-9]{4})\\)"
+  }
+  
+  tnt_info <- str_extract(proc_out, version_re) %>%
     na.omit() %>%
     str_match(version_re) %>%
     extract(1,-1) %>%
     as.list()
   tnt_info <- c(check_tnt_info, tnt_info)
-  cli_alert_info("Found TNT version {tnt_info$number} ({tnt_info$date}, {tnt_info$bits} bit)")
+  cli_alert_info("Found TNT version {tnt_info$number} ({tnt_info$date})")
 
   assign("tnt_info", tnt_info, envir = .envir)
   cli_alert_success("TNT executable verified and attached to {.val {format(.envir)}}.")
