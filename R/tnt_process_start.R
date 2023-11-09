d0ce98da60812872ebfe8c7ddee9fdc14571b819
Sienna Birch <tbroughm@une.edu.au>
2023-11-09 15:49:33 +1100

    Windows support for starting TNT process

--- R/tnt_process_start.R
+++ R/tnt_process_start.R
@@ -3,12 +3,13 @@
 #' @description
 #' \code{tnt_process_start} starts an instance of TNT.
 #' @param .envir The environment that TNT has been attached to.
+#' @param queue An optional \code{CommandQueue} object.
 #' @importFrom checkmate check_environment check_string test_list test_string
 #'   test_true
 #' @importFrom cli cli_abort
 #' @importFrom processx process
 #' @importFrom stringr str_extract str_match
-tnt_process_start <- function (.envir) {
+tnt_process_start <- function (.envir, queue = NULL) {
   val_check <- check_environment(.envir)
   if (!test_true(val_check)) {
     cli_abort(c("{.arg .envir} must be an environment.",
@@ -24,19 +25,29 @@ tnt_process_start <- function (.envir) {
     cli_abort(c("Could not find path to TNT process."))
   }
 
-  if (tnt_info$platform == "unix") {
-    tnt_process <- process$new(
-      tnt_info$path,
-      pty = TRUE,
-      env = c(COLUMNS = "10000", TERM = "xterm", HOME = Sys.getenv("HOME"))
-    )
+  proc_args <- list(
+    command = tnt_info$path,
+    stdin = NULL,
+    stdout = "|",
+    stderr = "|",
+    env = c("current", TERM = "xterm", HOME = Sys.getenv("HOME"))
+  )
+  
+  if (!test_null(queue)) {
+    tnt_tempfile <- tempfile(pattern = "nitro-", fileext = ".tnt")
+    all_cmds <- as.character(queue)
+    writeLines(all_cmds, tnt_tempfile)
+    proc_args$args <- glue("proc {tnt_tempfile};")
   } else {
-    cli_abort("Can't run subprocesses on non UNIX-like platforms.",
-              "x" = "{.pkg nitro} does not currently support running on non UNIX-like platforms (i.e., Windows).")
+    if (tnt_info$platform == "unix") {
+      proc_args$pty <- TRUE
+      stdin = "|"
+    }
   }
+  
+  tnt_process <- do.call(process$new, proc_args)
 
   tnt_info$process <- tnt_process
   assign("tnt_info", tnt_info, envir = .envir)
-
   return(invisible(TRUE))
 }
