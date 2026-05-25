625e62be56bd516fbd065119d4081e338f2729ab
paravian <contact@paravian.net>
2026-05-25 12:38:39 +1000

    Add get_package_objects dispatch command

---
+++ R/get_package_objects.R
@@ -0,0 +1,14 @@
+#' Get package objects
+#'
+#' @importFrom magrittr %>%
+get_package_objects <- function () {
+  fn_name <- sys.call() %>%
+    as.character()
+  pkg_name <- get(fn_name) %>%
+    environment() %>%
+    environmentName()
+  pkg_objs <- getNamespace(pkg_name) %>%
+    ls
+
+  pkg_objs
+}
